9d9a736481d5227b22a305269c0855cfa06ea20f
Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
2018-10-14 07:14:00 -0400

    Displays Ingredients

--- predict.js
+++ predict.js
@@ -1,14 +1,17 @@
-var myClarifaiApiKey = 'b2b715306e5847bba77d54cdfa208184';
-var myWolframAppId = 'WV4WPT-RTP2K5W3AV';
 
-var app = new Clarifai.App({apiKey: myClarifaiApiKey});
+const app = new Clarifai.App({
+  apiKey: '6e71d8ec52df43c2925ca1d3e551e140'
+ });
 
 /*
   Purpose: Pass information to other helper functions after a user clicks 'Predict'
   Args:
     value - Actual filename or URL
     source - 'url' or 'file'
+
     */
+
+    src="https://sdk.clarifai.com/js/clarifai-latest.js";
 function predict_click(value, source) {
   var preview = $(".food-photo");
   var file    = document.querySelector("input[type=file]").files[0];
@@ -23,7 +26,8 @@ function predict_click(value, source) {
 
   if (file) {
     reader.readAsDataURL(file);
-    $('#concepts').html('<img src="' + loader + '" class="loading" />');
+    //$('#concepts').html('<img src="' + loader + '" class="loading" />');
+     
   } else { alert("No file selcted!"); }
 }
 
@@ -32,16 +36,51 @@ function predict_click(value, source) {
   Args:
     value - Either {url : urlValue} or { base64 : base64Value }
 */
-function doPredict(value) {
+
+
+  
+    
+  function doPredict(value) {
+  app.models.predict("bd367be194cf45149e75f01d59f77ba7", value).then(
+  function(response)
+    {
+      var demoP = document.getElementById("d");
+      var html = "";
+      if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
+        var tag = response.rawData.outputs[0].data.concepts[0].name;
+        for(var i=0;i<response.rawData.outputs[0].data.concepts.length;i++)
+        {
+          var a =response.rawData.outputs[0].data.concepts[i].name;
+          html += a + "<br/>";
+                    
+                   
+          //document.getElementById("d").innerHTML = a;
+          //document.write('<p id="syndicated-content">'+ a +'</p>');
+          //document.write(a);
+        }
+        demoP.innerHTML = html;
+      
+      }
+
+      
+    },
+    function(err) {
+      // 
+      console.log(err);
+    }
+  );
+}
+
+/*function doPredict(value) {
   app.models.predict(Clarifai.FOOD_MODEL, value).then(function(response) {
       if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
         var tag = response.rawData.outputs[0].data.concepts[0].name;
-        var url = 'http://api.wolframalpha.com/v2/query?input='+tag+'%20nutrition%20facts&appid='+myWolframAppId;
-
-        getNutritionalInfo(url, function (result) {
-          $('#concepts').html('<h3>'+ tag + '</h3>' + "<img src='"+result+"'>");
-        });
-      }
+        for (var i = response.rawData.outputs[0].data.concepts.length - 1; i >= 0; i--) {
+          console.write(response.rawData.outputs[0].data.concepts[i].name);
+        }
+        console.write(tag);
+        }
     }, function(err) { console.log(err); }
   );
 }
+*/
